43cce222d0b9cca1506e0b98bc750e9ed7ca2075
ngicheru-netizen <n.gicheru@alustudent.com>
2026-05-08 18:12:41 +0300

    Added commas to puts expressions

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -3,5 +3,5 @@ File.readlines(ARGV[0]).each do |line|
 sender = line.scan(/\[from:([^\]]+)\]/).join 
 receiver = line.scan(/\[to:([^\]]+)\]/).join
 flags = line.scan(/\[flags:([^\]]+)\]/).join
-puts "#{sender} #{receiver} #{flags}"
+puts "#{sender}, #{receiver}, #{flags}"
 end
