6c7366b996ed5bef33d5d2420e44019d7470aefa
ngicheru-netizen <n.gicheru@alustudent.com>
2026-05-08 17:59:51 +0300

    changed expressions

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 File.readlines(ARGV[0]).each do |line|
-sender = line.scan(/(\[from:\+\d{11}\])/).join 
-receiver = line.scan(/(\[to:\+\d{11}\])/).join
-flags = line.scan(/\[flags:-(\d{0,2}):(\d{0,2}):-(\d{0,2}):(\d{0,2}):-(\d{0,2})]/).join
+sender = line.scan(/\[from:[^\]]+\]/).join 
+receiver = line.scan(/\[to:[^\]]+\]/).join
+flags = line.scan(/\[flags:[^\]]+\]/).join
+puts "#{sender} #{receiver} #{flags}"
 end
