3681fdcd20b9242ec019ff73b813d3b2c85ad806
ngicheru-netizen <n.gicheru@alustudent.com>
2026-05-08 18:10:33 +0300

    Added parenthesis to just get the values

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 File.readlines(ARGV[0]).each do |line|
-sender = line.scan(/\[from:[^\]]+\]/).join 
-receiver = line.scan(/\[to:[^\]]+\]/).join
-flags = line.scan(/\[flags:[^\]]+\]/).join
+sender = line.scan(/\[from:([^\]]+)\]/).join 
+receiver = line.scan(/\[to:([^\]]+)\]/).join
+flags = line.scan(/\[flags:([^\]]+)\]/).join
 puts "#{sender} #{receiver} #{flags}"
 end
