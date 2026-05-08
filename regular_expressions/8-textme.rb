0c0574d3666eeef0cff1c9fb902591833d2c502f
ngicheru-netizen <n.gicheru@alustudent.com>
2026-05-08 18:26:42 +0300

    changed 2nd line

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
-File.readlines(ARGV[0]).each do |line|
-sender = line.scan(/\[from:([^\]]+)\]/).join 
+line = ARGV[0]
+sender = line.scan(/\[from:([^\]]+)\]/).join
 receiver = line.scan(/\[to:([^\]]+)\]/).join
 flags = line.scan(/\[flags:([^\]]+)\]/).join
-puts "#{sender}, #{receiver}, #{flags}"
-end
+puts "#{sender},#{receiver},#{flags}"
