e671ada46cea1448ed535a87a029a8a7b428c12d
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-29 22:39:33 +0300

    1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Find top 10 hot posts of subreddit from reddit API"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    user_agent = "macos:com.intranet.apistuff:v1.0.0(by /u/PlasticDrummer2706)"
+    headers = {"User-Agent": user_agent}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        parsed_data = response.json()
+        hot_posts = parsed_data["data"]["children"]
+        top_ten_posts = hot_posts[:10]
+        return top_ten_posts
+
+
+if __name__ == "__main__":
+    print(top_ten("programming"))
